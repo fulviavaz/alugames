2e7d05cbee596c358d4e774ed0b4cb0a04e1491b
fulviavaz <fulviavaz@hotmail.com>
2023-12-19 15:45:16 -0300

    created lista alugados

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let listaALugados = [];
+
 function alterarStatus(id) {
   let gameClicado = document.getElementById(`game-${id}`);
   let imagem = gameClicado.querySelector(".dashboard__item__img");
@@ -8,9 +10,15 @@ function alterarStatus(id) {
     imagem.classList.remove("dashboard__item__img--rented");
     botao.classList.remove("dashboard__item__button--return");
     botao.textContent = "Alugar";
+    listaALugados = listaALugados.filter(item => item !== id); // Remova o jogo da lista ao devolver.
+    // console.log("foram alugados: ", listaALugados.length);
+    alert("O jogo foi devolvido com sucesso!");
   } else {
     imagem.classList.add("dashboard__item__img--rented");
     botao.classList.add("dashboard__item__button--return");
     botao.textContent = "Devolver";
+    listaALugados.push(id); // Adicione o jogo à lista ao alugar.
+    console.log("foram alugados: ", listaALugados.length);
+  
   }
 }
